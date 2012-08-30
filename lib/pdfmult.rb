8da6000469b603fef56b8ac1de3197c6ad448c97
Marcus Stollsteimer <sto.mar@web.de>
2012-08-30 20:29:01 +0200

    typo fixed

--- lib/pdfmult.rb
+++ lib/pdfmult.rb
@@ -71,7 +71,7 @@ module Pdfmult
   HOMEPAGE  = 'https://github.com/stomar/pdfmult/'
 
   COPYRIGHT = "Copyright (C) 2011-2012 Marcus Stollsteimer.\n" +
-              "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n" +
+              "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n" +
               "This is free software: you are free to change and redistribute it.\n" +
               "There is NO WARRANTY, to the extent permitted by law."
 
