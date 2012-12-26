386bbf5e6f624ea455966c41b17aab2ac23c9acc
Marcus Stollsteimer <sto.mar@web.de>
2012-12-26 09:54:46 +0100

    Remove duplicate code

--- test/test_latex_document.rb
+++ test/test_latex_document.rb
@@ -10,14 +10,14 @@ require 'pdfmult'
 describe Pdfmult::LaTeXDocument do
 
   it 'should return the expected LaTeX code for 4 pages' do
-    document = Pdfmult::LaTeXDocument.new('sample.pdf', 4, 3)
-    document.to_s.split(/\n/)[0].must_equal "\\documentclass[a4paper]{article}"
-    document.to_s.split(/\n/)[-2].must_equal "\\includepdf[pages={3,3,3,3},nup=2x2]{sample.pdf}%"
+    document_lines = Pdfmult::LaTeXDocument.new('sample.pdf', 4, 3).to_s.split(/\n/)
+    document_lines[0].must_equal  "\\documentclass[a4paper]{article}"
+    document_lines[-2].must_equal "\\includepdf[pages={3,3,3,3},nup=2x2]{sample.pdf}%"
   end
 
   it 'should return the expected LaTeX code for 8 pages' do
-    document = Pdfmult::LaTeXDocument.new('sample.pdf', 8, 3)
-    document.to_s.split(/\n/)[0].must_equal "\\documentclass[a4paper,landscape]{article}"
-    document.to_s.split(/\n/)[-2].must_equal "\\includepdf[pages={3,3,3,3,3,3,3,3},nup=4x2]{sample.pdf}%"
+    document_lines = Pdfmult::LaTeXDocument.new('sample.pdf', 8, 3).to_s.split(/\n/)
+    document_lines[0].must_equal  "\\documentclass[a4paper,landscape]{article}"
+    document_lines[-2].must_equal "\\includepdf[pages={3,3,3,3,3,3,3,3},nup=4x2]{sample.pdf}%"
   end
 end
