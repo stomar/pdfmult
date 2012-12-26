stomar/pdfmult

commit 3116d2641e4497d2fce13c2a979f4cb152ace812

avoid chained gsub calls

lib/pdfmult.rb
@@ -233,7 +233,10 @@ module Pdfmult
         geometry = '4x4'
       end
 
-      content_template = CONTENT.gsub(/PAGES/, page_string).gsub(/GEOMETRY/, geometry).gsub(/FILENAME/, @infile)
+      content_template = CONTENT.gsub(/PAGES|GEOMETRY|FILENAME/,
+                                      'PAGES' => page_string,
+                                      'GEOMETRY' => geometry,
+                                      'FILENAME' => @infile)
 
       content = HEADER.gsub(/CLASSOPTIONS/, class_options)
       @page_count.times do |i|

commit 6580a49d2ff20d9d836b4f304bf119843dda91de

Fix gsub with replacement hash for Ruby 1.8
Fix a regression that was introduced with commit 3116d2641e44:
in Ruby 1.8, String#gsub can not be used with replacement hash.

lib/pdfmult.rb
@@ -214,10 +214,14 @@ module Pdfmult
       class_options << ',landscape'  if @layout.landscape?
       page_string = 'PAGE,' * (@layout.pages - 1) + 'PAGE'  # 4 copies: e.g. 1,1,1,1
 
-      content_template = CONTENT.gsub(/PAGES|GEOMETRY|FILENAME/,
-                                      'PAGES' => page_string,
-                                      'GEOMETRY' => @layout.geometry,
-                                      'FILENAME' => @infile)
+      if RUBY_VERSION =~ /1\.8/
+        content_template = CONTENT.gsub('PAGES', page_string).gsub('GEOMETRY', @layout.geometry).gsub('FILENAME', @infile)
+      else
+        content_template = CONTENT.gsub(/PAGES|GEOMETRY|FILENAME/,
+                                        'PAGES' => page_string,
+                                        'GEOMETRY' => @layout.geometry,
+                                        'FILENAME' => @infile)
+      end
 
       content = HEADER.gsub(/CLASSOPTIONS/, class_options)
       @page_count.times do |i|