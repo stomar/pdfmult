663f49edb5d23fc9938f9747c39b375a073e7508
Marcus Stollsteimer <sto.mar@web.de>
2013-10-27 20:53:17 +0100

    Release 1.3.2

--- lib/pdfmult.rb
+++ lib/pdfmult.rb
@@ -29,8 +29,8 @@ require 'erb'
 module Pdfmult
 
   PROGNAME  = 'pdfmult'
-  VERSION   = '1.3.1'
-  DATE      = '2013-01-04'
+  VERSION   = '1.3.2'
+  DATE      = '2013-10-27'
   HOMEPAGE  = 'https://github.com/stomar/pdfmult/'
   TAGLINE   = 'puts multiple copies of a PDF page on one page'
 
